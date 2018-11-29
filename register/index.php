7a6298e5f774b57fe3e9431c169f81545ba1d800
pornpunnarai <kudokung_w@hotmail.com>
2018-11-29 18:30:37 +0700

    add register page

---
+++ register/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Spp - Master's Program </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- Custom Theme files -->
+    <link href="../css/register.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- //Custom Theme files -->
+    <!-- web font -->
+    <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+    <!-- //web font -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+</head>
+<style>
+    .input-container input{
+        background-color: #121212bd;
+        border-radius: 5px;
+    }
+
+    .input-container{
+        position: relative;
+
+    }
+
+    .input-container i{
+        position: relative;
+        left: 15px;
+        top: -30px;
+        color: white;
+    }
+
+</style>
+<body>
+<!-- main -->
+<div class="main wrapper" style="background: rgba(0, 0, 0, 0.6); height: 100vh;">
+
+    <div class="main-agileinfo">
+        <div class="agileits-top">
+            <p style="text-align: center; padding: 3rem;"><img src="../img/logo.png" width="100%"></p>
+
+            <form action="#" method="post">
+
+                <div class="input-container">
+                    <input class="text" type="email" name="email" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email" required="">
+                    <i class="fas fa-user" aria-hidden="true"></i>
+                </div>
+
+                <div class="input-container">
+                    <input class="text" type="password" name="password" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password" required="">
+                    <i class="fas fa-lock" aria-hidden="true"></i>
+                </div>
+
+                <div class="wthree-text" style="text-align: right">
+<!--                    <label class="anim">-->
+<!--                        <input type="checkbox" class="checkbox" required="">-->
+<!--                        <span>Forget password ?</span>-->
+<!--                    </label>-->
+<!--                    <div class="clear"> </div>-->
+                    <a href="#" style="color: white">Forget password ?</a>
+                </div>
+
+                <input type="submit" value="SIGN IN">
+            </form>
+            <p>Get New Account ...<a href="register.php" style="color: #04B1DC"><b>SIGN UP</b></a></p>
+        </div>
+    </div>
+
+</div>
+<!-- //main -->
+</body>
+</html>
---
+++ register/register.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Spp - Master's Program </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- Custom Theme files -->
+    <link href="../css/register.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- //Custom Theme files -->
+    <!-- web font -->
+    <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+    <!-- //web font -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+</head>
+<body>
+<!-- main -->
+<div class="main wrapper" style="background: rgba(0, 0, 0, 0.6); height: 100vh;">
+
+
+    <div class="main-agileinfo">
+
+        <div class="agileits-top">
+<!--            <p style="text-align: center; padding: 3rem;"><img src="../img/logo.png" width="100%"></p>-->
+<!---->
+<!--            <form action="#" method="post">-->
+<!---->
+<!--                <div class="input-container">-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-md-6">-->
+<!--                            <input class="text" type="password" name="password" placeholder="Password" required="">-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6"></div>-->
+<!--                    </div>-->
+<!--                    -->
+<!--                </div>-->
+<!---->
+<!--                <input type="submit" value="SIGN IN">-->
+<!---->
+<!--            </form>-->
+<!--            <p>Get New Account ...<a href="#" style="color: #04B1DC"><b>SIGN UP</b></a></p>-->
+
+
+        </div>
+    </div>
+
+</div>
+<!-- //main -->
+</body>
+</html>
