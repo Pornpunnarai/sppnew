afebbfc978f57a4f0f65a27fcab8d4dcb1213380
pornpunnarai <kudokung_w@hotmail.com>
2018-12-07 12:13:30 +0700

    fix style on mobile

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 <head>
     <title>School of Public Policy</title>
     <?php include 'head-part.php'?>
+
+    <style>
+        @media only screen and (min-width: 1281px) {
+            p.cmu-text{
+                font-size: 24px;
+            }
+
+            .spp-text-s{
+                font-size: 72px;
+            }
+
+            .spp-text-pp{
+                font-size: 72px;
+                padding-left: 2vw;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -28,84 +45,125 @@
 
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="img/Desktop-Background/HomePage1.png" alt="First slide" style="filter: brightness(65%);">
+                    <img class="d-block w-100" src="img/Desktop-Background/HomePage2.png" alt="Second slide" style="filter: brightness(80%);">
+                    <div class="carousel-caption d-md-block spp-block">
+                        <div class="row index-1-caption-1">
+                            <p  class="cmu-text">Chiang Mai University</p>
+                        </div>
 
-                <div class="carousel-caption d-md-block">
-                    <div class="row" style="padding-top: 10px;">
-                        <img src="img/Rectangle%2054@2x.png" width="184px" style="height: 12px;margin-top: 2vh;">
-                        <b style="font-size: 24px;padding-left: 2vw;">Our Purpose</b>
-                    </div>
-                    <div class="row" style="margin-top: 2%">
-                        <b style="font-size: 72px">inclusive</b>
-                    </div>
-                    <div class="row">
-                        <b style="font-size: 72px;">innovation</b>
-                    </div>
-                    <div class="row">
-                        <b style="font-size: 72px;">Progressive</b>
-                    </div>
-                </div>
-            </div>
+                        <div class="row index-1-caption-2">
+                            <b class="spp-text-s">School of</b>
+                        </div>
 
-            <div class="carousel-item">
-                <img class="d-block w-100" src="img/Desktop-Background/HomePage2.png" alt="Second slide" style="filter: brightness(80%);">
-                <div class="carousel-caption d-md-block spp-block">
-                    <div class="row">
-                        <p  style="font-size: 24px">Chiang Mai University</p>
-                    </div>
+                        <div class="row index-1-caption-3">
+                            <img class="img-padding-index-1" src="img/Rectangle%2054@2x.png">
+                            <b class="spp-text-pp">Public Policy</b>
+                        </div>
 
-                    <div class="row" style="justify-content: flex-start; margin-top: -3vh;">
-                        <b style="font-size: 72px">School of</b>
-                    </div>
+                        <div class="row index-1-caption-mobile">
+                            <img class="img-padding-index-1" src="img/Rectangle%2054@2x.png">
+                            <b class="spp-text-pp">School of Public Policy</b>
+                        </div>
+                        <div id="index-mobile">
+                            <div class="col-xs-12">
+                                <div class="index-section2-1-caption-1">
+                                    <p class="our-purpose">Our Purpose</p>
+                                </div>
 
-                    <div class="row" style="float: left; margin-left: -8vw;">
-                        <img src="img/Rectangle%2054@2x.png" width="250px" style="height: 15px;margin-top: 5vh;">
-                        <b style="font-size: 72px;padding-left: 2vw;">Public Policy</b>
-                    </div>
-                </div>
-            </div>
+                                <div class="index-section2-1-caption-2">
+                                    <img class="img-padding-index-1" src="img/Rectangle%2054@2x.png">
+                                </div>
 
-            <div class="carousel-item">
-                <img class="d-block w-100" src="img/Desktop-Background/HomePage3.png" alt="Third slide" style="filter: brightness(70%);">
-                <div class="carousel-caption d-md-block policy-market-block">
-                    <div class="row" style="float: left; margin-left: -8vw;">
-                        <img src="img/Rectangle%2054@2x.png" width="250px" style="height: 15px;margin-top: 9vh; ">
-                        <div class="row" style="padding-left: 3vw; text-align: left;">
-                            <p><b style="font-size: 24px;">Policy Market EP : 4</b><br>
-                            <b style="font-size: 72px;">Differently Abled</b><br>
-                            <b style="font-size: 24px;">: New Paradigms for Inclusive Higher Education</b><br>
-                            <b style="font-size: 24px;">6 - 7 December 2018</b><br>
-                            <b style="font-size: 24px;">At Northern Science Park, Chiang Mai, Thailand</b><br>
-
-                                <button class="btn register" style="height: 4vh;margin-top: 4vh;">Register now</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <button class="btn register" style="height: 4vh;margin-top: 4vh;">Download Material</button>
-                            </p>
+                                <div class="index-section2-1-caption-3">
+                                    <b class="our-purpose-detail">Inclusive<br>Innovative<br>Progressive</b>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+
+
+<!--                <div id="index-mobile">-->
+<!--                    <div class="col-xs-12">-->
+<!--                        <div class="index-section2-1-caption-1">-->
+<!--                            <p class="our-purpose">Our Purpose</p>-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="index-section2-1-caption-2">-->
+<!--                            <img class="img-padding-index-1" src="img/Rectangle%2054@2x.png">-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="index-section2-1-caption-3">-->
+<!--                            <b class="our-purpose-detail">Inclusive<br>Innovative<br>Progressive</b>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+
+
             </div>
 
-            <div class="carousel-item">
-                <img class="d-block w-100" src="img/Desktop-Background/HomePage4.png" alt="Fourth slide" style="filter: brightness(70%);">
-                <div class="carousel-caption d-md-block phd-block">
-
-                    <div class="row" style="justify-content: flex-start; margin-top: -5vh;">
-                        <b style="font-size: 72px">PhD & Master</b>
-                    </div>
-
-                    <div class="row" style="float: left; margin-left: -15vw;">
-                        <img src="img/Rectangle%2054@2x.png" width="250px" style="height: 15px;margin-top: 6vh; ">
-                        <b style="font-size: 72px;padding-left: 2vw;">in Public Policy</b>
-
-                        <button class="btn register" style="height: 4vh;    margin-top: 5vh; margin-left: 5vw;">Register now</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button class="btn register" style="height: 4vh;    margin-top: 5vh;">Download Material</button>
-                    </div>
-
-                    <div class="row" style="float: left; margin-left: 19vw;padding-top: 1vh">
-                        <b style="font-size: 18px;">Learn more <i class="fa fal fa-arrow-right"></i></b>
-                    </div>
-                </div>
-            </div>
+<!--            <div class="carousel-item">-->
+<!--                <img class="d-block w-100" src="img/Desktop-Background/HomePage1.png" alt="First slide" style="filter: brightness(65%);">-->
+<!---->
+<!--                <div class="carousel-caption d-md-block">-->
+<!--                    <div class="row" style="padding-top: 10px;">-->
+<!--                        <img src="img/Rectangle%2054@2x.png" width="184px" style="height: 12px;margin-top: 2vh;">-->
+<!--                        <b  style=" font-size: 24px;padding-left: 2vw;">Our Purpose</b>-->
+<!--                    </div>-->
+<!--                    <div class="row" style="margin-top: 2%">-->
+<!--                        <b style="font-size: 72px">inclusive</b>-->
+<!--                    </div>-->
+<!--                    <div class="row">-->
+<!--                        <b style="font-size: 72px;">innovation</b>-->
+<!--                    </div>-->
+<!--                    <div class="row">-->
+<!--                        <b style="font-size: 72px;">Progressive</b>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="carousel-item">-->
+<!--                <img class="d-block w-100" src="img/Desktop-Background/HomePage3.png" alt="Third slide" style="filter: brightness(70%);">-->
+<!--                <div class="carousel-caption d-md-block policy-market-block">-->
+<!--                    <div class="row" style="float: left; margin-left: -8vw;">-->
+<!--                        <img src="img/Rectangle%2054@2x.png" width="250px" style="height: 15px;margin-top: 9vh; ">-->
+<!--                        <div class="row" style="padding-left: 3vw; text-align: left;">-->
+<!--                            <p><b style="font-size: 24px;">Policy Market EP : 4</b><br>-->
+<!--                            <b style="font-size: 72px;">Differently Abled</b><br>-->
+<!--                            <b style="font-size: 24px;">: New Paradigms for Inclusive Higher Education</b><br>-->
+<!--                            <b style="font-size: 24px;">6 - 7 December 2018</b><br>-->
+<!--                            <b style="font-size: 24px;">At Northern Science Park, Chiang Mai, Thailand</b><br>-->
+<!---->
+<!--                                <button class="btn register" style="height: 4vh;margin-top: 4vh;">Register now</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-->
+<!--                                <button class="btn register" style="height: 4vh;margin-top: 4vh;">Download Material</button>-->
+<!--                            </p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="carousel-item">-->
+<!--                <img class="d-block w-100" src="img/Desktop-Background/HomePage4.png" alt="Fourth slide" style="filter: brightness(70%);">-->
+<!--                <div class="carousel-caption d-md-block phd-block">-->
+<!---->
+<!--                    <div class="row" style="justify-content: flex-start; margin-top: -5vh;">-->
+<!--                        <b style="font-size: 72px">PhD & Master</b>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="row" style="float: left; margin-left: -15vw;">-->
+<!--                        <img src="img/Rectangle%2054@2x.png" width="250px" style="height: 15px;margin-top: 6vh; ">-->
+<!--                        <b style="font-size: 72px;padding-left: 2vw;">in Public Policy</b>-->
+<!---->
+<!--                        <button class="btn register" style="height: 4vh;    margin-top: 5vh; margin-left: 5vw;">Register now</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-->
+<!--                        <button class="btn register" style="height: 4vh;    margin-top: 5vh;">Download Material</button>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="row" style="float: left; margin-left: 19vw;padding-top: 1vh">-->
+<!--                        <b style="font-size: 18px;">Learn more <i class="fa fal fa-arrow-right"></i></b>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
             <div class="row event-last-research">
                 <div class="up-coming-event-block" style="text-align: left">
