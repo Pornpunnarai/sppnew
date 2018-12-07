87440f11bb3ad0af35cfb0fdbf8bd98e3a765cce
paruj <paruj_lertjiranuwat@cmu.ac.th>
2018-12-07 10:08:24 +0700

    back-end almost done

--- register/application-form.php
+++ register/application-form.php
@@ -290,220 +290,10 @@
                 <?php include 'student_personal_details.php'?>
                 <?php include 'student_contact_details.php'?>
                 <?php include 'student_emergency_contact.php'?>
-
-                <fieldset>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-11 text-box" style="text-align: left">
-                                Please tell us why you choose  a Public Policy Program ?
-                            </div>
-                            <div class="col-lg-11">
-                                <input class="text" type="text" name="firstname" required="">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row" style="text-align: left">
-                        <div class="col-lg-11">
-                            <p style="font-family: lato-bold; font-size: 24px;color: white;">Curriculum vitae<br>
-                                <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">+Add file</button>
-                                <small style="font-style: italic; font-size: 11px; font-family: lato-light;">Please insert file format PDF, MS Word Doc, JPEG, PNG. Limits file size : 20 MB</small>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="row" style="position: absolute;margin-left: -23vw;text-align: left;width: 10%; margin-top: -25vh;">
-                        <p style="font-size: 24px; color: white; font-family: lato-bold;writing-mode: vertical-rl;transform: rotate(180deg);">
-                            MOTIVATION LETTER</p>
-                    </div>
-
-                    <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-
-                    <p style="font-style: italic; font-size: 14px; font-family: lato-light; color: white;">
-                            It should be around 500 - 1,000 words in length, covering your skills,
-                            experiences relevant to field of public policy, your ambitions,
-                            motivations and interest in the public policy program
-                    </p>
-                    <input type="button" name="next" class="next action-button" value="Next"/>
-
-                </fieldset>
-
-                <fieldset>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Institution / School / College
-                            </div>
-                            <div class="col-lg-8">
-                                <input class="text" type="text" name="firstname" required="">
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                City / Country
-                            </div>
-                            <div class="col-lg-8">
-                                <input class="text" type="text" name="middlename" required="">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="input-container" style="    margin-bottom: 10px;">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Select Qualification
-                            </div>
-                            <div class="col-lg-8">
-                                <div class="col-lg-3">
-                                    <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                        Bachelor’s
-                                    </button>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Subject
-                            </div>
-                            <div class="col-lg-8">
-                                <input class="text" type="text" name="familyname" required="">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Thesis Title ( if applicable )
-                            </div>
-                            <div class="col-lg-8">
-                                <input class="text" type="text" name="familyname" required="">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="input-container" style="    margin-bottom: 10px;">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Thesis abstract file
-                            </div>
-                            <div class="col-lg-8">
-                                <div class="col-lg-3">
-                                    <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                        +Add file
-                                    </button>
-                                </div>
-                                <div class="col-lg-8">
-                                    <small style="font-style: italic; color: white">Please insert file format PDF, MS Word Doc, JPEG, PNG</small>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="input-container" style="    margin-bottom: 10px;">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Academic certificates
-                            </div>
-                            <div class="col-lg-8">
-                                <div class="col-lg-3">
-                                    <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                        +Add file
-                                    </button>
-                                </div>
-
-                                <div class="col-lg-8">
-                                    <small style="font-style: italic; color: white">Please insert file format PDF, MS Word Doc, JPEG, PNG</small>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Start Date
-                            </div>
-                            <div class="col-lg-8">
-                                <div class="col-lg-4">
-                                    <input class="text" type="text" name="conutryofbirth" required="">
-                                </div>
-                                <div class="col-lg-4 text-box">
-                                    End Date<small style="font-style: italic">(Expected) </small>
-                                </div>
-                                <div class="col-lg-4">
-                                    <input class="text" type="text" name="conutryofbirth" required="">
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="input-container">
-                        <div class="row">
-                            <div class="col-lg-3 text-box">
-                                Graduation Date<small style="font-style: italic">(Expected) </small>
-                            </div>
-                            <div class="col-lg-8">
-                                <div class="col-lg-4">
-                                    <input class="text" type="text" name="conutryofbirth" required="">
-                                </div>
-                                <div class="col-lg-4 text-box">
-                                    Official Duration
-                                </div>
-                                <div class="col-lg-4">
-                                    <input class="text" type="text" name="conutryofbirth" placeholder="Year" required="">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row" style="position: absolute;margin-left: 40vw;text-align: left;width: 100%; margin-top: -30vh;">
-                        <p style="font-family: lato-bold; font-size: 24px;color: white;">
-                            <button class="btn btn-primary" style="background-color: #121212bd; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                Qualifications 1
-                            </button>
-
-                            <button class="btn btn-primary" style="background-color: #121212bd; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                Qualifications 2
-                            </button>
+                <?php include 'student_motivation_letter.php'?>
+                <?php include 'student_academic_qualification.php'?>
 
 
-                            <button class="btn btn-primary" style="background-color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                Qualifications 3
-                            </button>
-
-                            <button class="btn btn-primary" style="background-color: #F15C2B; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
-                                + Qualifications
-                            </button>
-                        </p>
-                    </div>
-
-                    <div class="row" style="position: absolute;margin-left: -23vw;text-align: left;width: 100%; margin-top: -40vh;">
-                        <p style="font-size: 24px; color: white; font-family: lato-bold;writing-mode: vertical-rl;transform: rotate(180deg);">ACADEMIC QUALIFICATION</p>
-                    </div>
-
-                    <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-
-                    <p style="font-style: italic; font-size: 14px; font-family: lato-light; color: white;">
-                        Please provide details and attach the degree certificate/diploma
-                        as well as transcript of the relevant previous degree(s) you obtained, starting with the most recent one.
-                    </p>
-                    <input type="button" name="next" class="next action-button" value="Next"/>
-
-                </fieldset>
 
                 <fieldset>
 
@@ -1327,6 +1117,57 @@
                         type: 'POST',
                         url: 'check-add.php',
                         data: JSON.stringify(student_emergency_contact),
+                        contentType: 'application/json',
+                        success: function(msg){
+                            console.log(msg);
+                        }
+
+                    });
+
+                }
+
+            </script>
+            <script>
+                <?php
+                include '../connect-mysql.php';
+                $sql = "SELECT * FROM `student_motivation_letter` WHERE `student_account_id` = '1'";
+                $objQuery = mysqli_query($objCon, $sql);
+                $result = mysqli_fetch_array($objQuery, MYSQLI_ASSOC);
+                $json = json_encode($result);
+                ?>
+
+                var json = '<?=$json?>';
+                var obj = JSON.parse(json);
+
+                document.getElementById("detail").value = obj.detail;
+
+
+
+
+
+
+
+                document.getElementById("student_motivation_letter").onchange = function() {student_emergency_contact()};
+                function student_emergency_contact() {
+
+                    var detail = document.getElementById("detail").value;
+
+
+
+
+                    var student_motivation_letter = { "student_motivation_letter": [
+                            {
+                                "student_account_id": obj.student_account_id,
+                                "detail": detail
+                            }
+                        ]};
+                    console.log(JSON.stringify(student_motivation_letter));
+
+
+                    $.ajax({
+                        type: 'POST',
+                        url: 'check-add.php',
+                        data: JSON.stringify(student_motivation_letter),
                         contentType: 'application/json',
                         success: function(msg){
                             console.log(msg);
---
+++ register/student_academic_qualification.php
@@ -0,0 +1,175 @@
+<fieldset>
+
+    <div class="input-container">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Institution / School / College
+            </div>
+            <div class="col-lg-8">
+                <input class="text" type="text" name="firstname" required="">
+            </div>
+        </div>
+
+    </div>
+
+    <div class="input-container">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                City / Country
+            </div>
+            <div class="col-lg-8">
+                <input class="text" type="text" name="middlename" required="">
+            </div>
+        </div>
+    </div>
+
+    <div class="input-container" style="    margin-bottom: 10px;">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Select Qualification
+            </div>
+            <div class="col-lg-8">
+                <div class="col-lg-3">
+                    <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                        Bachelor’s
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="input-container">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Subject
+            </div>
+            <div class="col-lg-8">
+                <input class="text" type="text" name="familyname" required="">
+            </div>
+        </div>
+    </div>
+
+    <div class="input-container">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Thesis Title ( if applicable )
+            </div>
+            <div class="col-lg-8">
+                <input class="text" type="text" name="familyname" required="">
+            </div>
+        </div>
+    </div>
+
+    <div class="input-container" style="    margin-bottom: 10px;">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Thesis abstract file
+            </div>
+            <div class="col-lg-8">
+                <div class="col-lg-3">
+                    <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                        +Add file
+                    </button>
+                </div>
+                <div class="col-lg-8">
+                    <small style="font-style: italic; color: white">Please insert file format PDF, MS Word Doc, JPEG, PNG</small>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="input-container" style="    margin-bottom: 10px;">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Academic certificates
+            </div>
+            <div class="col-lg-8">
+                <div class="col-lg-3">
+                    <button class="btn btn-primary" style="color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                        +Add file
+                    </button>
+                </div>
+
+                <div class="col-lg-8">
+                    <small style="font-style: italic; color: white">Please insert file format PDF, MS Word Doc, JPEG, PNG</small>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="input-container">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Start Date
+            </div>
+            <div class="col-lg-8">
+                <div class="col-lg-4">
+                    <input class="text" type="text" name="conutryofbirth" required="">
+                </div>
+                <div class="col-lg-4 text-box">
+                    End Date<small style="font-style: italic">(Expected) </small>
+                </div>
+                <div class="col-lg-4">
+                    <input class="text" type="text" name="conutryofbirth" required="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="input-container">
+        <div class="row">
+            <div class="col-lg-3 text-box">
+                Graduation Date<small style="font-style: italic">(Expected) </small>
+            </div>
+            <div class="col-lg-8">
+                <div class="col-lg-4">
+                    <input class="text" type="text" name="conutryofbirth" required="">
+                </div>
+                <div class="col-lg-4 text-box">
+                    Official Duration
+                </div>
+                <div class="col-lg-4">
+                    <input class="text" type="text" name="conutryofbirth" placeholder="Year" required="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" style="position: absolute;margin-left: 40vw;text-align: left;width: 100%; margin-top: -30vh;">
+        <p style="font-family: lato-bold; font-size: 24px;color: white;">
+            <button class="btn btn-primary" style="background-color: #121212bd; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                Qualifications 1
+            </button>
+
+            <button class="btn btn-primary" style="background-color: #121212bd; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                Qualifications 2
+            </button>
+
+
+            <button class="btn btn-primary" style="background-color: #04B1DC; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                Qualifications 3
+            </button>
+
+            <button class="btn btn-primary" style="background-color: #F15C2B; font-size: 13px;font-family: lato-regular;color: white;border-radius: 25px;">
+                + Qualifications
+            </button>
+        </p>
+    </div>
+
+    <div class="row" style="position: absolute;margin-left: -23vw;text-align: left;width: 100%; margin-top: -40vh;">
+        <p style="font-size: 24px; color: white; font-family: lato-bold;writing-mode: vertical-rl;transform: rotate(180deg);">ACADEMIC QUALIFICATION</p>
+    </div>
+
+    <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+
+    <p style="font-style: italic; font-size: 14px; font-family: lato-light; color: white;">
+        Please provide details and attach the degree certificate/diploma
+        as well as transcript of the relevant previous degree(s) you obtained, starting with the most recent one.
+    </p>
+    <input type="button" name="next" class="next action-button" value="Next"/>
+
+</fieldset>
